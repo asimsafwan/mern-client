60b6a5b467dd8be7d2960dd5947ab1a8db41952e
Asim Safwan <asim.safwan@gmail.com>
2021-07-15 09:43:02 +0530

    added build

---
+++ build/precache-manifest.0c5f4a5a8b002237fdd7cabf99fd384d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890a7f7d9b49bdbffb271ffeea527999",
+    "url": "/index.html"
+  },
+  {
+    "revision": "191264b5487a63f64cba",
+    "url": "/static/css/main.00cf29bb.chunk.css"
+  },
+  {
+    "revision": "5e76206b98d86d623ac5",
+    "url": "/static/js/2.13a3b436.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.13a3b436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191264b5487a63f64cba",
+    "url": "/static/js/main.ace89999.chunk.js"
+  },
+  {
+    "revision": "2b55614ca07f93359093",
+    "url": "/static/js/runtime-main.bfd8333c.js"
+  }
+]);
